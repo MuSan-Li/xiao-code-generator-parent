b80d852c8fa2df5f7ebaf7c74589cd7a632d9fea
MuSan-Li <446796145@qq.com>
2024-02-25 15:56:37 +0800

    存储优化

--- code-generator-web-backend/src/main/java/cn/xiao/cg/mapper/GeneratorMapper.java
+++ code-generator-web-backend/src/main/java/cn/xiao/cg/mapper/GeneratorMapper.java
@@ -2,6 +2,9 @@ package cn.xiao.cg.mapper;
 
 import cn.xiao.cg.model.entity.Generator;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 /**
  * 代码生成器信息 Mapper
@@ -10,6 +13,8 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  */
 public interface GeneratorMapper extends BaseMapper<Generator> {
 
+    @Select("SELECT id, dist_path FROM t_generator WHERE is_delete = 1")
+    List<Generator> listDeletedGenerator();
 }
 
 
